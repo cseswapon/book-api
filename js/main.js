b1d1b26696dcc04633b7ff25b34efc8665c3f62d
cseswapon <cseswaponsaha@gmail.com>
2021-09-02 13:55:30 +0600

    no image show

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+// button click //
 const searchBook = () =>
 {
     const inputText = document.getElementById('input-text');
     const inputTextValue = inputText.value;
     inputText.value = '';
-    if (inputTextValue === '') {
+    if (inputTextValue === '') {    // empty error handlig
         document.getElementById('empty').style.display = 'block';
         document.getElementById('card-display').style.display = 'none';
         document.getElementById('about-result').style.display = 'none';
@@ -16,11 +17,11 @@ const searchBook = () =>
             .then(data => booksearch(data.docs))
     }
 }
-
+// api call with function //
 const booksearch = data =>
 {
     // console.log(data);
-    if (data.length === 0) {
+    if (data.length === 0) {    // erro massage handeling
         document.getElementById('error').style.display = 'block';
         document.getElementById('about-result').style.display = 'none';
         document.getElementById('card-display').style.display = 'none';
@@ -37,31 +38,20 @@ const booksearch = data =>
         {
             // console.log(book);
             // console.log(book.text[2]);
+            // card ready with deynamic //
             const div = document.createElement('div');
             div.classList.add('col');
-            /* div.innerHTML = `
-        <div class="card h-100">
-        <img src="https://covers.openlibrary.org/b/id/${book?.cover_i}-L.jpg" class="card-img-top img-fluid img-thumbnail" style="
-        height: 450px; alt="Cover Page Is Not Found">
-        <div class="card-body">
-            <h5 class="card-title">${book?.text[2]}</h5>
-            <p class="card-text"><span><strong>Author : </strong></span>${book?.author_name}</p><hr>
-            <p class="card-text">
-                <span><strong>1<sup>st</sup> Publish : </strong>${book?.first_publish_year}</span>
-            </p>
-        </div>
-    </div>
-        `; */
             div.innerHTML = `
             <div class="card h-100">
-            <img src="https://covers.openlibrary.org/b/id/${book?.cover_i}-L.jpg" class="card-img-top img-fluid mx-auto h-50" alt="Book img not found">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : '10909258'}-L.jpg" class="card-img-top img-fluid mx-auto h-50" alt="Book img not found">
             <div class="card-body">
                 <h5 class="card-title">${book?.text[2]}</h5><hr>
-                <p class="card-text"><span><strong>Author : </strong></span>${book?.author_name}</p>
+                <p class="card-text"><span><strong>Author : </strong></span>${book.author_name ? book.author_name : 'not found'}</p>
+                <p class="card-text"><span><strong>Publisher : </span>${book.publisher ? book.publisher : 'not found'} </strong></p>
             </div>
             <div class="card-footer">
                 <small class="text-muted">
-                <span><strong>1<sup>st</sup> Publish : </strong>${book?.first_publish_year}</span>
+                <span><strong>1<sup>st</sup> Publish year : </strong>${book.first_publish_year ? book.first_publish_year : ' '}</span>
                 </small>
             </div>
         </div>
